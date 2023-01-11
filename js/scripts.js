b1174263470c2ee047a04157c9292feb4d982106
James Provance <jb24x7@gmail.com>
2023-01-11 10:59:15 -0800

    initial commit
    Co-authored-by: Brent Hickman <brenthickman@ymail.com>

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+
+
+// window.onload = function() {
+//   let form = document.querySelector("form");
+//   form.onsubmit = function(event) {
+//     event.preventDefault();
+//     let input = document.getElementById("userInput").value;
+
+//     document.querySelector("span#userInput").innerText = input;
+
+//     document.querySelector("div#shout").removeAttribute("class");
+
+    
+//   };
+
+
+// };
+
+
+window.onload = function() {
+
+  let form = document.querySelector("form")
+
+  form.onsubmit = function(event) {
+    event.preventDefault();
+    let input = document.getElementById("userInput").value;
+
+    document.querySelector("span#userInput").innerText = input;
+
+    document.querySelector("div#shout").removeAttribute("class");
+    
+
+  };
+}
